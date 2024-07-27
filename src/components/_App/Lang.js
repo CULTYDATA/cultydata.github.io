4d929e68113bb6ca724f4f5b79a14c75c0fc0e47
Farhath Ayesha <farhath.ayesha@carbonplace.com>
2024-07-27 16:42:04 +0530

    language switch

--- src/components/_App/Lang.js
+++ src/components/_App/Lang.js
@@ -1,29 +1,47 @@
-import React from 'react';
-import { graphql } from 'gatsby';
-import { Link, useI18next } from 'gatsby-plugin-react-i18next';
+import React, { useState } from "react"
+import { Link, useI18next } from "gatsby-plugin-react-i18next"
 
 const Lang = ({ title }) => {
-    const { languages, originalPath } = useI18next();
-    return (
-        <><h1 style={{ margin: 0 }}>
-            <Link
-                to="/"
-                style={{
-                    color: `white`,
-                    textDecoration: `none`
-                }}>
-                {title}
-            </Link>
-        </h1><ul className="languages">
-                {languages.map((lng) => (
-                    <li key={lng}>
-                        <Link to={originalPath} language={lng}>
-                            {lng}
-                        </Link>
-                    </li>
-                ))}
-            </ul></>
-    );
-};
+  const { languages, originalPath, changeLanguage, language } = useI18next()
+  const [isOpen, setIsOpen] = useState(false)
 
-export default Lang;
+  const handleLanguageChange = lng => {
+    changeLanguage(lng)
+    setIsOpen(false)
+  }
+
+  const languageNames = {
+    en: "English - EN",
+    fr: "Français - FR",
+  }
+
+  const shortNames = {
+    en: "EN",
+    fr: "FR",
+  }
+
+  return (
+    <div className="lang-container">
+      <h1 className="lang-title">
+        <Link to="/">{title}</Link>
+      </h1>
+      <button
+        className="lang-button bx bx-globe"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {shortNames[language]} <i className="bx bx-chevron-down"></i>
+      </button>
+      {isOpen && (
+        <ul className="lang-dropdown">
+          {languages.map(lng => (
+            <li key={lng} onClick={() => handleLanguageChange(lng)}>
+              {languageNames[lng]}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  )
+}
+
+export default Lang
